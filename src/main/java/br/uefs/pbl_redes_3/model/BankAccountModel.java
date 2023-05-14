6e35610a78ee6b0adcc410457aa69e283cec2980
Sergivaldo <sergivaldo123@gmail.com>
2023-05-13 21:16:12 -0300

    -add: lombok

--- src/main/java/br/uefs/pbl_redes_3/model/BankAccountModel.java
+++ src/main/java/br/uefs/pbl_redes_3/model/BankAccountModel.java
@@ -1,7 +1,12 @@
 package br.uefs.pbl_redes_3.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
+@Getter
+@Setter
 public class BankAccountModel {
     private UUID id;
 }
--- src/main/java/br/uefs/pbl_redes_3/model/ClientModel.java
+++ src/main/java/br/uefs/pbl_redes_3/model/ClientModel.java
@@ -1,7 +1,12 @@
 package br.uefs.pbl_redes_3.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
+@Getter
+@Setter
 public class ClientModel {
     private UUID id;
 }
